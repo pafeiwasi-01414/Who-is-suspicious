ea9a9d698cb8bbe08b0079af4e9b30a34b78a7e5
pafeiwasi-01414 <palwasi01414@gmail.com>
2026-05-09 16:27:20 +0900

    あったほうがよさそうななにか

---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+[BepInPlugin("com.wis.addon", "wis", "1.0.2.0")]
+public class Main : BasePlugin
+{
+    public override void Load()
+    {
+        Hori.Core.PatchManager.Init();
+    }
+}
